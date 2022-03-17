04bfa485614993dde3e8cee95f401b23adf31874
DORA3301 <101839244+DORA3301@users.noreply.github.com>
2022-03-17 23:40:07 +0100

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,21 @@
+
+secret_word = "Python"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+    if guess_count < guess_limit and guess != secret_word:
+        print("Wrong guess, try again.")
+
+
+if out_of_guesses:
+    print("YOU LOST!")
+else:
+    print("YOU WIN!")
---
+++ Translator.py
@@ -0,0 +1,16 @@
+# Translation where vowels are changed with certain letter/s
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "Z"
+            else:
+                translation = translation + "z"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("Enter your phrase: ")))
